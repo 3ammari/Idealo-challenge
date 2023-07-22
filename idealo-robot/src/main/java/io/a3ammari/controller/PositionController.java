bc82ca8f7727ccda96c1ba2d3646741b2e0a1c4e
3ammari <me@3ammari.io>
2023-07-22 12:12:16 +0200

    added controllers, LogBack.xml

---
+++ idealo-robot/src/main/java/io/a3ammari/controller/PositionController.java
@@ -0,0 +1,32 @@
+package io.a3ammari.controller;
+
+import io.a3ammari.model.Command;
+import io.a3ammari.model.Position;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class PositionController {
+
+    private Logger logger = LoggerFactory.getLogger(PositionController.class);
+    @PostMapping("/evaluate")
+    public ResponseEntity<Position> evaluateEndpoint(@RequestBody String commands){
+        final String TAG = "evaluateEndpoint ";
+        logger.debug(TAG+commands);
+        Position position = null;
+        var cmd = commands.lines().map(Command::parse).toList();
+        for (Command c:
+             cmd) {
+            position = c.execute(position);
+        }
+        return ResponseEntity.ok(position);
+    }
+
+}
--- idealo-robot/src/main/java/io/a3ammari/model/Command.java
+++ idealo-robot/src/main/java/io/a3ammari/model/Command.java
@@ -4,6 +4,25 @@ package io.a3ammari.model;
 
 public sealed interface Command permits Init,Forward,TurnAround,Wait,Right,Left {
     Position execute(Position position);
+
+    static Command parse(String s){
+        var parts = s.split(" ");
+        return switch (parts[0]){
+            case "POSITION" -> new Init(
+                    new Position(
+                            Integer.parseInt(parts[1]),
+                            Integer.parseInt(parts[2]),
+                            Direction.valueOf(parts[3])
+                    )
+            );
+            case "FORWARD" -> new Forward(Integer.parseInt(parts[1]));
+            case "WAIT" -> new Wait();
+            case "TURNAROUND" -> new TurnAround();
+            case "LEFT" -> new Left();
+            case "RIGHT" -> new Right();
+            default -> throw new IllegalArgumentException("Unexpected value: " + s);
+        };
+    }
 }
 
 
